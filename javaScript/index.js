a288abb10100192201a1cebf9e62801bd50bac23
niyobertin <niyonkurubbertin@gmail.com>
2024-02-19 14:52:56 +0200

    log in and sign up page

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,12 +1,29 @@
-
+let menuList = document.getElementById("link");
  function showhide()
 {
-  let menuList = document.getElementById("link");
-    if (menuList.style.display !== "none") {
-        menuList.style.display = "none";
-    }
-    else {
-      menuList.style.transition = "1s"; 
+    if (menuList.style.display === "none") {
         menuList.style.display = "block";
     }
+    else {
+        menuList.style.display = "none";
+    }
 }
+
+window.onscroll =() => stickyNav();//excute the stickyNav on scroll
+const navbar = document.getElementById("head");
+const sticky = navbar.offsetTop;
+const  stickyNav =()=> {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// function hideIt(){
+//   let nav=document.getElementById('link')
+//   if(nav.style.display=="" || nav.style.display=='block')
+//   nav.style.display = 'none'
+//   else nav.style.display='block'
+//   }
+//   document.getElementById('link').addEventListener('click',hideIt)
