8f3eb4d80e0d82e1a8d8ae7c929a5eef790c994c
niyobertin <niyonkurubbertin@gmail.com>
2024-02-29 00:46:18 +0200

    single brog view

---
+++ local.storage/numbers.js
@@ -0,0 +1,10 @@
+
+const querriesFromLocalStorage = JSON.parse(window.localStorage.getItem("querries"));
+console.log(querriesFromLocalStorage);
+const dataFromLocalStorage = JSON.parse(window.localStorage.getItem("blogs"));
+console.log(dataFromLocalStorage);
+let querries = document.getElementById('total-querry');
+querries.innerHTML = querriesFromLocalStorage.length + ' ';
+let articals = document.getElementById('total-artical');
+articals.innerHTML = dataFromLocalStorage.length + ' ';
+
--- local.storage/querries.js
+++ local.storage/querries.js
@@ -10,10 +10,20 @@ for(let i = 0;i < querriesFromLocalStorage.length;i++){
     const names = document.createTextNode(querriesFromLocalStorage[i].name);
     const message = document.createElement('p');
     const messages = document.createTextNode(querriesFromLocalStorage[i].message);
+    const delete_button = document.createElement('button');
+    delete_button.id ='delete';
+    const replay_button = document.createElement('button');
+    replay_button.id = 'replay';
+    delete_button.innerHTML = 'Delete';
+    replay_button.innerHTML = 'Replay';
+
+
 
     name.appendChild(names);
     message.appendChild(messages);
     newQuerry.appendChild(name);
     newQuerry.appendChild(message);
+    newQuerry.appendChild(replay_button);
+    newQuerry.appendChild(delete_button)
     querryDiv.appendChild(newQuerry);
 }
