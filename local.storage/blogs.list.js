8c718b97df0c6f4a8b1b8797e9fb314c52cfd436
Niyonkuru Bertin <85437905+niyobertin@users.noreply.github.com>
2024-04-02 23:59:33 +0200

    Update blogs.list.js

--- local.storage/blogs.list.js
+++ local.storage/blogs.list.js
@@ -46,7 +46,7 @@ try{
             const head = document.createElement("h2");
             const content = document.createElement("p");
             const headings =  document.createTextNode(blogsList[i].title);
-            const textContent = document.createTextNode((blogsList[i].content).replace(/<[^>]*>?/gm, '').slice(0,100) +"...");
+            const textContent = document.createTextNode((blogsList[i].content).replace(/<[^>]*>?/gm, '').slice(0,200) +"...");
             div.appendChild(blogImages);
             head.appendChild(headings)
             div.appendChild(head);
